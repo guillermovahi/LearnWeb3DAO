44fe1e2717a1fb5ff31fa01413cb575f7d4d89b1
Guillermo Vargas Hidalgo <guillermovahi@gmail.com>
2022-10-02 15:18:23 +0200

    Full NFT project developed

---
+++ Sophomore/NFT-Collection/hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xBB80cBeD9e957bDdf1567A285Ff0D878aF37f24D";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
